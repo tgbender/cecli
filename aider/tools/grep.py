cead8bb1f9aeb5d66647f4a449dc7b5977825b5c
Dustin Washington <dwash96@gmail.com>
2025-12-09 20:02:51 -0500

    rg specific override is not needed with double dash syntax

--- aider/tools/grep.py
+++ aider/tools/grep.py
@@ -166,11 +166,6 @@ class Tool(BaseTool):
             if tool_name == "grep":
                 cmd_args.append("--exclude-dir=.git")
 
-            # Pattern may start with a dash, this will make rg interpret it as
-            # an option, "-e" works around this.
-            if tool_name == "rg":
-                cmd_args.extend(["-e"])
-
             # Add pattern and directory path
             cmd_args.extend(["--", pattern, str(search_dir_path)])
 
